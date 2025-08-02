7a0a21fcb1ae1c202bb7e3c6cc79a05f12393af9
Bobbili Ammulu <145207267+22K61A4413@users.noreply.github.com>
2025-08-02 21:12:38 +0530

    Add files via upload

---
+++ Swap two numbers without temp variable.py
@@ -0,0 +1,6 @@
+print("Swap two numbers without temp variable")
+first_num=int(input("Enter first num: "))
+second_num=int(input("Enter second num: "))
+print("Before swapping: ",first_num,second_num)
+first_num,second_num=second_num,first_num
+print("After swapping: ",first_num,second_num)
