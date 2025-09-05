562774c800a58d9aac920ea6d5ba9534e43b85ed
Bobbili Ammulu <145207267+22K61A4413@users.noreply.github.com>
2025-09-05 10:42:29 +0530

    Update ASCII value of a character.py

--- ASCII value of a character.py
+++ ASCII value of a character.py
@@ -1,3 +1,4 @@
 character=input("Enter a character: ")
 ascii_value=ord(character)
-print(f"The Ascii value for the {character} value is: {ascii_value}")
+
+print(f"The Ascii value for the {character} value is: {ascii_value}")   
