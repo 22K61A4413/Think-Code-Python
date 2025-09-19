732e9d34dcda4fe6fd40090475333ec1b34042dc
Bobbili Ammulu <145207267+22K61A4413@users.noreply.github.com>
2025-09-19 10:50:29 +0530

    Update Anagram check.py

--- Anagram check.py
+++ Anagram check.py
@@ -4,5 +4,4 @@ anagram_check=sorted(str1)==sorted(str2)
 if anagram_check==True:
     print(f"The given two strings: {str1},{str2} is an anagrams")
 else:
-
-    print(f"The given two strings: {str1},{str2} is not an anagrams") 
+    print(f"The given two strings: {str1},{str2} is not an anagrams")
